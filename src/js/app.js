bbc47341ba7326f1767526f12de8875fecb3b376
Ekaterina Volkova <volkovakaterin@gmail.com>
2021-05-20 20:31:11 +0400

    app changed

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,14 @@
-/*eslint-disable no-unused-vars*/
-// TODO: write code here
 
+// TODO: write code here
 // comment this to pass build
+
+/*eslint-disable no-unused-vars*/
 const unusedVariable = 'variable';
+/*eslint-enable no-unused-vars*/
 
 // for demonstration purpose only
 export default function demo(value) {
   return value;
 }
 
-console.log('app.js included');
-/*eslint-enable no-unused-vars*/
+console.log('app.js included'); /*eslint-disable-line no-console*/
